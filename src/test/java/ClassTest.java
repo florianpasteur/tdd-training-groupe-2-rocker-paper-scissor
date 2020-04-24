dba5daf27263fab2f8538cd66f7f55abbe28e433
Florian PASTEUR <florian@hackages.io>
2020-04-24 14:16:26 +0200

    Initial Commit: Boilerplate for Java 13 Maven Project with Unit Tests

---
+++ src/test/java/ClassTest.java
@@ -0,0 +1,11 @@
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class ClassTest {
+
+    @Test
+    void should_test() {
+        assertThat(true).isEqualTo(true);
+    }
+}
