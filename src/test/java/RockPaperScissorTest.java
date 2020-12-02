ab35b0a4aa61f4efc3b8588703bfd72972f65a87
Florian PASTEUR <florian@hackages.io>
2020-12-02 10:38:39 +0100

    Scissor beats Rock

--- src/test/java/RockPaperScissorTest.java
+++ src/test/java/RockPaperScissorTest.java
@@ -5,10 +5,10 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class RockPaperScissorTest {
 
     /*
-      P1: Paper, P2: Rock => P1 Wins
-      P1: Paper, P2: Scissor => P2 Wins
-      P1: Paper, P2: Paper => Tie
-      P1: Rock, P2: Scissor => P1 Wins
+      P1: Paper, P2: Rock => P1 Wins - Done
+      P1: Paper, P2: Scissor => P2 Wins - Done
+      P1: Paper, P2: Paper => Tie - Done
+      P1: Rock, P2: Scissor => P1 Wins - Skipped
       P1: Scissor, P2: Rock => P2 Wins
      */
 
@@ -46,5 +46,17 @@ public class RockPaperScissorTest {
 
         // Then
         assertThat(GameResult.TIE).isEqualTo(gameResult);
+    }
+
+    @Test
+    void should_return_P2_wins_when_P1_plays_SCISSOR_and_P2_plays_ROCK() {
+        // Given
+        Game game = new Game();
+
+        // When
+        GameResult gameResult = game.play(Sign.SCISSOR, Sign.ROCK);
+
+        // Then
+        assertThat(GameResult.P2_WINS).isEqualTo(gameResult);
     }
 }
