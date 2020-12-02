5454fc26e32afa15e3948be74e16b746a05706a6
Florian PASTEUR <florian@hackages.io>
2020-12-02 10:47:46 +0100

    Refactor

--- src/main/java/Game.java
+++ src/main/java/Game.java
@@ -1,17 +1,15 @@
 public class Game {
     public GameResult play(Sign player1sign, Sign player2sign) {
-        if (player1sign == Sign.ROCK && player2sign == Sign.PAPER) {
-            return  GameResult.P2_WINS;
-        }
-        if (player1sign == Sign.SCISSOR && player2sign == Sign.ROCK) {
-            return  GameResult.P2_WINS;
+        if (beats(player1sign, player2sign)) {
+            return GameResult.P2_WINS;
         }
         if (player1sign == Sign.PAPER && player2sign == Sign.PAPER) {
             return GameResult.TIE;
         }
-        if (player1sign == Sign.PAPER && player2sign == Sign.SCISSOR) {
-            return GameResult.P2_WINS;
-        }
         return GameResult.P1_WINS;
+    }
+
+    private boolean beats(Sign player1sign, Sign player2sign) {
+        return player1sign.beats(player2sign);
     }
 }
--- src/main/java/Sign.java
+++ src/main/java/Sign.java
@@ -1,3 +1,13 @@
 public enum Sign {
-    ROCK, SCISSOR, PAPER
+    ROCK, SCISSOR, PAPER;
+
+    public boolean beats(Sign player2sign) {
+        if (this == ROCK && player2sign == PAPER)
+            return  true;
+        if (this == SCISSOR && player2sign == ROCK)
+            return  true;
+        if (this == PAPER && player2sign == SCISSOR)
+            return  true;
+        return false;
+    }
 }
